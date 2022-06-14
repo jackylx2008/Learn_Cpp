a7a2cc0e622a32a3f806da02a81b4766c54f2e13
jackylx2008 <jackylx2008@hotmail.com>
2022-06-14 09:32:43 +0800

    2022-06-14

--- 2022-06-09_.cpp
+++ 2022-06-09_.cpp
@@ -6,10 +6,10 @@
 // 学习STL--3 vector 容器
 class Person {
   public:
-    // Person() {
-    //     this->m_name = "None";
-    //     this->m_age = 0;
-    // }
+    Person() {
+        this->m_name = "None";
+        this->m_age = 0;
+    }
     Person(std::string name, int age) {
         this->m_name = name;
         this->m_age = age;
@@ -17,6 +17,8 @@ class Person {
     std::string m_name;
     int m_age;
 };
+
+// size and capacity
 void test01() {
     std::vector<Person> v;
     v.push_back(Person("liu", 10));
@@ -37,8 +39,63 @@ void test01() {
     std::cout << "v.capacity" << v.capacity() << std::endl;
     std::cout << "v.size" << v.size() << std::endl;
 }
+
+// insert
+void test02() {
+    std::vector<Person> v;
+    v.push_back(Person("liu", 10));
+    v.push_back(Person("xin", 20));
+    v.push_back(Person("luo", 50));
+    v.push_back(Person("ya", 90));
+    Person temp = Person("NO", 100);
+    v.insert(v.begin() + 2, 2, temp);
+    for (std::vector<Person>::iterator i = v.begin(); i != v.end(); i++) {
+        std::cout << "v[i] = " << (*i).m_name << std::endl;
+        std::cout << "v[i] = " << (*i).m_age << std::endl;
+    }
+}
+
+// 读取元素 at []
+void test03() {
+    std::vector<Person> v;
+    v.push_back(Person("liu", 10));
+    v.push_back(Person("xin", 20));
+    v.push_back(Person("luo", 50));
+    v.push_back(Person("ya", 90));
+    for (int i = 0; i < v.size(); i++) {
+        std::cout << "v.name = " << v[i].m_name << std::endl;
+    }
+    for (int i = 0; i < v.size(); i++) {
+        std::cout << "v.age = " << v.at(i).m_age << std::endl;
+    }
+}
+
+// swap
+void test04() {
+    std::vector<int> v;
+    for (int i = 0; i < 100000; i++) {
+        v.push_back(i);
+    }
+    std::cout << "v.size = " << v.size() << std::endl;
+    std::cout << "v.capacity = " << v.capacity() << std::endl;
+
+    v.resize(3);
+
+    std::cout << "v.size = " << v.size() << std::endl;
+    std::cout << "v.capacity = " << v.capacity() << std::endl;
+
+    // 利用swap收缩内存.先构造匿名函数,进行swap后收缩内存,匿名函数语句结束后被系统自动回收
+    std::vector<int>(v).swap(v);
+    // std::vector<int> t(v);
+    // v.swap(t);
+    // std::cout << "v.size = " << v.size() << std::endl;
+    // std::cout << "v.capacity = " << v.capacity() << std::endl;
+}
 int main(int argc, char *argv[]) {
-    test01();
+    // test01();
+    // test02();
+    // test03();
+    test04();
 
     return 0;
 }
